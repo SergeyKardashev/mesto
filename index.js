ec4837c2a35901440fbc3ceb780a2cdf83e221e1
SergeyKardashev <angeldeep@yandex.ru>
2023-03-21 18:02:27 +0700

    feat: доделал скрипт, поправил адаптив у профиля

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
-// кнопка редактирования профиля
-const profileEditBtn = document.querySelector(".profile__edit-btn");
-// проверка переменной через консоль
-// console.log(profileEditBtn);
+// кнопка редактирования профиля и проверка переменной через консоль
+const btnProfile = document.querySelector(".profile__edit-btn");
+// console.log(`Profile button element: ${btnProfile}`);
 
 // весь попап профиля
-const popupProfileEdit = document.querySelector(".popup_profile-edit");
+const popupProfile = document.querySelector(".popup_profile");
+// console.log(popupProfile);
+
+// ищу html элемент с ФИО в тексте профиля (объект или узел дом-дерева)
+let userNameElement = document.querySelector(".profile__name");
+// проверяю его ли я нашел console.log(userNameTag);
+
+// ищу html-элемент с должностью в тексте профиля (объект или узел дом-дерева)
+let userJobElement = document.querySelector(".profile__job");
+// console.log(userJobElement); // проверяю его ли я нашел
+
+// Находим форму в DOM +  проверяю ее ли я нашел. Нужна отдельно форма, а не весь попап, так как буду обращаться к нему для отправки формы.
+let formElement = popupProfile.querySelector(".popup__form_type_profile");
+// console.log(`Form: ${formElement}`);
+
+// ищу поле ввода ФИО + проверяю его ли я нашел
+let userNameInput = formElement.querySelector(".popup__input_user-name");
+// console.log(`FIO input: ${userNameInput}`);
+
+// ищу поле ввода работы + проверяю его ли я нашел
+let userJobInput = formElement.querySelector(".popup__input_user-job");
+// console.log(`Job Input: ${userJobInput}`);
 
 // крестик - кнопка закрытия попапа профиля
-const popupCloseBtn = document.querySelector(".popup__close-button");
+const btnPopupProfileClose = popupProfile.querySelector(".popup__close-button");
 
-profileEditBtn.addEventListener("click", function () {
-  // проверка на отлов клика
-  // console.log("clicked");
-  // добавление класса ОТКРЫТО для попапа
-  popupProfileEdit.classList.add("popup_open");
+// клик по кнопке редактирования профиля + проверка на отлов клика
+btnProfile.addEventListener("click", function () {
+  // console.log("clicked btnProfile");
+
+  // Заполняю поля формы значениями из объектов страницы
+  userNameInput.value = userNameElement.textContent;
+  userJobInput.value = userJobElement.textContent;
+
+  popupProfile.classList.add("popup_open"); // добавление класса ОТКРЫТО для попапа
 });
 
-// клик по крестику - кнопка закрытия попапа профиля
-// убираю класс, делающий попап видимым
-popupCloseBtn.addEventListener("click", function () {
-  popupProfileEdit.classList.remove("popup_open");
+// клик по крестику - кнопке закрытия попапа профиля
+btnPopupProfileClose.addEventListener("click", function () {
+  // проверка на отлов клика console.log("clicked closed");
+
+  popupProfile.classList.remove("popup_open"); // убираю класс, делающий попап видимым
 });
 
-// ищу поле ввода фамилии
-let inputUserName = popupProfileEdit.querySelector(".popup__input_user-name");
-// проверяю его ли я нашел
-// console.log(inputUserName);
+// Обработчик «отправки» формы (без вызова, просто функция)
+function handleFormSubmit(evt) {
+  evt.preventDefault();
 
-// ищу html-тег с фамилией в тексте профиля
-let userNameTag = document.querySelector(".profile__name");
-// проверяю его ли я нашел
-// console.log(userNameTag);
+  userNameElement.textContent = userNameInput.value;
+  userJobElement.textContent = userJobInput.value;
+  popupProfile.classList.remove("popup_open");
+}
 
-// Ищу значение ФИО внутри тега. Его можно брать НЕ из переменной.
-// let userNameValue = userNameTag.textContent;
-// Проверяю его ли я нашел
-// console.log(userNameValue);
-
-// Записываю в поля ввода значения из страницы
-// записываю значение из тега с именем сразу в поле ввода без лишней переменной userNameValue
-
-// Ищу значение поля ввода и проверяю его ли я нашел
-// console.log(inputUserName.value);
-
-// Записываю внутрь значения поля новое текстовое содержимое переменной, хранящей thml-тег с ФИО
-inputUserName.value = userNameTag.textContent;
+// Клик по кнопке СОХРАНИТЬ
+// 1) добавляю обработчик к ФОРМЕ (не к кнопке):
+// 1) он будет следить за событием “submit”, а НЕ "клик"
+popupProfile.addEventListener("submit", handleFormSubmit);
