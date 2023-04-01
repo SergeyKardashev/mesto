26ab6f6d51bd9da09dca7da2372cc1cce98fb45a
SergeyKardashev <63510751+SergeyKardashev@users.noreply.github.com>
2023-04-01 17:40:25 +0700

    Revert "Feature/create cards by js from template"

--- scripts/index.js
+++ scripts/index.js
@@ -14,8 +14,7 @@ let userNameElement = document.querySelector(".profile__name");
 let userJobElement = document.querySelector(".profile__job");
 // console.log(userJobElement); // проверяю его ли я нашел
 
-// Находим форму в DOM +  проверяю ее ли я нашел. Нужна отдельно форма, а не весь попап,
-// так как буду обращаться к нему для отправки формы.
+// Находим форму в DOM +  проверяю ее ли я нашел. Нужна отдельно форма, а не весь попап, так как буду обращаться к нему для отправки формы.
 let formElement = popupProfile.querySelector(".popup__form");
 // console.log(`Form: ${formElement}`);
 
@@ -68,72 +67,3 @@ function handleFormSubmit(evt) {
 // 2) Он следит за событием “submit”, а НЕ "клик"
 // 3) Он запустит функцию с букетом инструкций, а не одно действие.
 formElement.addEventListener("submit", handleFormSubmit);
-//
-//
-//
-//
-// Наполнить галлерею карточками мест на основе шаблона и массива
-
-// массив для изначального наполнения галлереи
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    alt: "Архыз",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    alt: "Челябинская область",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    alt: "Иваново",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    alt: "Камчатка",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    alt: "Холмогорский район",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    alt: "Байкал",
-  },
-];
-
-// скрипт по наполнению галлереи на основе вышеупомянутого массива:
-// alt tag is empty
-// template id is #card, gallery container class is .gallery
-
-// получаю содержимое шаблона в переменную
-const cardTemplate = document.querySelector("#card").content;
-// или по без квериселектора то же самое можно сделать const cardTemplate = document.getElementById("card");
-console.log(cardTemplate); // проверяю что нашел то что нужно выводя в консоль
-
-// в переменную сохраняю html-элемент, принимающий новорожденные карточки.
-const gallery = document.querySelector(".gallery");
-console.log(gallery); // проверяю что нашел то что нужно выводя в консоль
-
-// теперь проделать все то же самое циклом с остальными элементами массива. Столько раз, сколько элементов.
-
-initialCards.forEach((item, index) => {
-  // клонирую содержимое шаблона из переменной cardTemplate в переменную cardElement,
-  // найдя по классу контейнера внутри шаблона, а не по элементу шаблона.
-  const cardElement = cardTemplate.querySelector(".gallery__element").cloneNode(true);
-
-  // наполняю содержимым каждый тег клона отдельно
-  cardElement.querySelector(".gallery__img").src = initialCards[index].link;
-  cardElement.querySelector(".gallery__img").alt = initialCards[index].alt;
-  cardElement.querySelector(".gallery__text").textContent = initialCards[index].name;
-
-  // передаю это в DOM
-  // в объект-приемник добавляю карточку (в конец объекта)
-  gallery.append(cardElement);
-});
